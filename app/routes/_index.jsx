6a12cb90c53009392d4443fde98a3ef06571db03
jkkicks <jkkicks1@gmail.com>
2025-11-24 19:36:08 -0800

    Init Remix Project

---
+++ app/routes/_index.jsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", padding: "2rem" }}>
+      <h1>Hello World!</h1>
+      <p>Welcome to Remix.</p>
+    </div>
+  );
+}
